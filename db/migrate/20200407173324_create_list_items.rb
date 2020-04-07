6da3d1585ad0cceddc0f5e88aa67eb03663e10d8
shanelonergan <sptlonergan@gmail.com>
2020-04-07 13:39:16 -0400

    first migration

--- db/migrate/20200407173324_create_list_items.rb
+++ db/migrate/20200407173324_create_list_items.rb
@@ -2,7 +2,7 @@ class CreateListItems < ActiveRecord::Migration[6.0]
   def change
     create_table :list_items do |t|
       t.string :name
-      t.int :rank
+      t.integer :rank
       t.belongs_to :list, null: false, foreign_key: true
 
       t.timestamps
