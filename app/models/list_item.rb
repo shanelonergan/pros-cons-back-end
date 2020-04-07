1506a146e58d38062da1500d616c62c0b9026e69
shanelonergan <sptlonergan@gmail.com>
2020-04-07 14:30:26 -0400

    added type validation to list_item

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -1,4 +1,5 @@
 class ListItem < ApplicationRecord
+  # type keeps track of whether it is a pro or a con. true = pro, false = con
   belongs_to :list
-  validates :name, presence: true
+  validates :name, :type, presence: true
 end
