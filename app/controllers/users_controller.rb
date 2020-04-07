cb8db5169145be083040c43618192963f8315d8f
shanelonergan <sptlonergan@gmail.com>
2020-04-07 14:53:25 -0400

    working on user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,16 @@ Dotenv.load
 
 class UsersController < ApplicationController
     def create
+        user = User.create(user_params)
 
+        if user.valid?
+            user = user
+            token = JWT.encode({user_id: user.id}, ENV[JWT_SECRET_KEY], 'HS256')
     end
 
     def show
-
+        user = User.find_by(params[:id])
+        render json: user
     end
 
     def update
@@ -20,5 +25,7 @@ class UsersController < ApplicationController
 
     private
 
-    user_params = params.permit(:username)
+    def user_params
+        params.permit(:username, :email, :password_digest)
+    end
 end
