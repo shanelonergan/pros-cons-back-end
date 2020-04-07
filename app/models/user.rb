89770d1811e0d04033e4fcde0ce7a2a5b5f6a033
shanelonergan <sptlonergan@gmail.com>
2020-04-07 13:47:45 -0400

    added has_many lists and validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :lists
+    validates :username, :email, presence: true
+    validates :username, length: { minimum: 3 }
+    validates :email, :username, uniqueness: true
 end
