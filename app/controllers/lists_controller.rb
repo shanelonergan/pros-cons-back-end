6d240f49f88a75790267a92283c98345c373d978
shanelonergan <sptlonergan@gmail.com>
2020-05-26 12:20:24 -0400

    updated list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,25 @@
 class ListsController < ApplicationController
+    def show
+        list = List.find(params[:id])
+        render json: listing
+    end
+
+    def create
+        list = List.create(list_params)
+
+        if list.valid?
+            render json: list
+        else
+            render json: {errors: list.errors.full_messages}
+        end
+    end
+
+    def index
+        render json: Listing.all
+    end
+
+    private
+
+    def list_params
+        params.permit(:name)
 end
